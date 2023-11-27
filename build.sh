810726e4209acc6f8ebbfcf6c531f54e059671fb
Shivam Rajput <shivam.rajput@thoughtworks.com>
2023-11-27 14:12:25 +0530

    Shivi | update registry url

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-tag=$(curl https://hub.docker.com/v2/repositories/shiviraj/cloud-ui/tags | jq -r '.results | sort_by(.last_updated) | last(.[]).name')
+#tag=$(curl https://hub.docker.com/v2/repositories/shiviraj/cloud-ui/tags | jq -r '.results | sort_by(.last_updated) | last(.[]).name')
+tag=0.0
 echo current tag: $tag
 majorTag=$(echo $tag | cut -d '.' -f 1-2 )
 minorTag=$(echo $tag | cut -d '.' -f 3)
@@ -13,4 +14,4 @@ npm install && \
 npm run build --omit=dev && \
 rm -rf node_modules && \
 npm install --production --ignore-scripts --prefer-offline && \
-docker buildx build --no-cache --platform=linux/arm64,linux/amd64 -t shiviraj/cloud-ui:$newTag -t shiviraj/cloud-ui:latest --push .
+docker buildx build --no-cache --platform=linux/arm64,linux/amd64 -t shiviraj/cloud-ui:latest -t shiviraj/cloud-ui:$newTag --push .
