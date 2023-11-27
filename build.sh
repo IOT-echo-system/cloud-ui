4413b328172837a120247285eff0dbe015ec5145
Shivam Rajput <shivam.rajput@thoughtworks.com>
2023-11-27 14:56:28 +0530

    Shivi | change order for image tag.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-tag=$(curl https://hub.docker.com/v2/repositories/shiviraj/cloud-ui/tags | jq -r '.results | sort_by(.last_updated) | last(.[]).name')
+tag=$(curl https://hub.docker.com/v2/repositories/shiviraj/cloud-ui/tags | jq -r '.results | sort_by(.last_updated) | last(.[-2]).name')
 echo current tag: $tag
 majorTag=$(echo $tag | cut -d '.' -f 1-2 )
 minorTag=$(echo $tag | cut -d '.' -f 3)
-((minorTag++))
+((minorTag+=1))
 
 newTag=$(echo $majorTag.$minorTag)
 echo new tag: $newTag
@@ -13,4 +13,4 @@ npm install && \
 npm run build --omit=dev && \
 rm -rf node_modules && \
 npm install --production --ignore-scripts --prefer-offline && \
-docker buildx build --no-cache --platform=linux/arm64,linux/amd64 -t shiviraj/cloud-ui:latest -t shiviraj/cloud-ui:$newTag --push .
+docker buildx build --no-cache --platform=linux/arm64,linux/amd64 -t shiviraj/cloud-ui:$newTag -t shiviraj/cloud-ui:latest --push .
