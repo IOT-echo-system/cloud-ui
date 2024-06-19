44b7192f90955c11ea7b0a8cbe6fa0ed30e0f449
Shivam Rajput <raj.shiviraj@gmail.com>
2024-06-19 15:50:24 +0530

    Shivi | Show level as background color.

--- src/components/organisms/widgets/levelMonitor/LevelMonitor.tsx
+++ src/components/organisms/widgets/levelMonitor/LevelMonitor.tsx
@@ -6,7 +6,7 @@ import {LevelMonitorSetupValues} from './LevelMonitorSetupValues'
 import {LevelMonitorDisplay} from './LevelMonitorDisplay'
 import {Stack} from '@mui/material'
 
-export const LevelMonitor: React.FC<WidgetPropsType<'LEVEL_MONITOR'>> = ({widget: levelMonitorWidget}) => {
+export const LevelMonitor: React.FC<WidgetPropsType<'LEVEL_MONITOR'>> = ({widget}) => {
   const [setupMode, setSetupMode] = useState(false)
 
   const toggleSetupMode = () => {
@@ -14,22 +14,39 @@ export const LevelMonitor: React.FC<WidgetPropsType<'LEVEL_MONITOR'>> = ({widget
   }
 
   return (
-    <WidgetContainer p={2} spacing={2} sx={{minWidth: {xs: '90%', sm: 280}, minHeight: 320}}>
-      <Stack spacing={2}>
-        <WidgetTitle widget={levelMonitorWidget} />
-        <PolicyAllowed policyId={PolicyUtils.WIDGET_LEVEL_MONITOR_UPDATE} direction={'row'}>
-          <Button onClick={toggleSetupMode} variant={'contained'}>
-            {setupMode ? 'Disable' : 'Enable'} setup mode
-          </Button>
-        </PolicyAllowed>
-      </Stack>
-      <Stack height={'100%'} justifyContent={'center'}>
-        {setupMode ? (
-          <LevelMonitorSetupValues widget={levelMonitorWidget} />
-        ) : (
-          <LevelMonitorDisplay widget={levelMonitorWidget} />
-        )}
-      </Stack>
-    </WidgetContainer>
+    <Stack sx={{position: 'relative'}}>
+      <WidgetContainer spacing={2} sx={{minWidth: {xs: '90%', sm: 280}, minHeight: 320}}>
+        <Stack spacing={2} sx={{zIndex: 2}}>
+          <WidgetTitle widget={widget} />
+          <PolicyAllowed policyId={PolicyUtils.WIDGET_LEVEL_MONITOR_UPDATE} direction={'row'}>
+            <Button onClick={toggleSetupMode} variant={'contained'}>
+              {setupMode ? 'Disable' : 'Enable'} setup mode
+            </Button>
+          </PolicyAllowed>
+        </Stack>
+        <Stack height={'100%'} justifyContent={'center'} sx={{zIndex: 2}}>
+          {setupMode ? <LevelMonitorSetupValues widget={widget} /> : <LevelMonitorDisplay widget={widget} />}
+        </Stack>
+      </WidgetContainer>
+      <Stack
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          bottom: 0,
+          bgcolor: 'lightblue',
+          height: `${((widget.actualValue - widget.minValue) * 100) / (widget.maxValue - widget.minValue)}%`,
+          zIndex: 0,
+          borderBottomLeftRadius: '8px',
+          borderBottomRightRadius: '8px'
+        }}
+      ></Stack>
+    </Stack>
   )
 }
+
+/*
+5-------------25
+      10
+10-5/25-5
+
+ */
--- src/components/organisms/widgets/levelMonitor/LevelMonitorDisplay.tsx
+++ src/components/organisms/widgets/levelMonitor/LevelMonitorDisplay.tsx
@@ -5,9 +5,11 @@ import {Stack, Typography} from '@mui/material'
 export const LevelMonitorDisplay: React.FC<WidgetPropsType<'LEVEL_MONITOR'>> = ({widget: levelMonitorWidget}) => {
   return (
     <Stack direction={'row'} spacing={2} justifyContent={'center'} alignItems={'center'}>
-      <Typography p={'16px 32px'} bgcolor={'paper'}>
-        {levelMonitorWidget.value.toFixed(2)} {levelMonitorWidget.symbol}
-      </Typography>
+      <Stack bgcolor={'background.paper'} borderRadius={2}>
+        <Typography p={'8px 16px'}>
+          {levelMonitorWidget.value.toFixed(2)} {levelMonitorWidget.symbol}
+        </Typography>
+      </Stack>
     </Stack>
   )
 }
