b09cc7778689c566d991553dd539e1c92e134c1a
Shivam Rajput <raj.shiviraj@gmail.com>
2025-01-13 08:46:11 +0530

    Shivi | add conditional x-premises-path in headers.

--- src/services/webClient.ts
+++ src/services/webClient.ts
@@ -4,13 +4,17 @@ import {getStorage, StorageKeys} from '../utils/storage'
 import {apiConfig} from '../config/apiConfig'
 import {v4 as uuidV4} from 'uuid'
 
+const premisesPaths = ['/zones', '/boards', '/feeds', '/widgets']
+
 WebClient.interceptor.request(config => {
   const {token} = getStorage<{token: string}>(StorageKeys.AUTH) ?? {token: ''}
   const {premisesId} = getStorage<{premisesId: string}>(StorageKeys.PREMISES_ID) ?? {premisesId: ''}
   config.baseURL = apiConfig.baseUrl
   config.headers.authorization = token
   config.headers['x-trace-id'] = uuidV4()
-  config.headers['x-premises-id'] = premisesId
+  if (premisesPaths.some(path => config.url?.startsWith(path))) {
+    config.headers['x-premises-id'] = premisesId
+  }
   return config
 })
 
